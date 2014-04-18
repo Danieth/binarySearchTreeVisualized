0ecde993060592d10873edc3b53406d3d398c30c
Richard Harrah <topplethenunnery@gmail.com>
2014-04-18 16:59:53 -0400

    adding spaces into state names where necessary

--- Person.java
+++ Person.java
@@ -46,16 +46,16 @@ public class Person {
         STATE_MAP.put("Arkansas", "AR");
         STATE_MAP.put("Nevada", "NV");
         STATE_MAP.put("California", "CA");
-        STATE_MAP.put("NewHampshire", "NH");
+        STATE_MAP.put("New Hampshire", "NH");
         STATE_MAP.put("Colorado", "CO");
-        STATE_MAP.put("NewJersey", "NJ");
+        STATE_MAP.put("New Jersey", "NJ");
         STATE_MAP.put("Connecticut", "CT");
-        STATE_MAP.put("NewMexico", "NM");
+        STATE_MAP.put("New Mexico", "NM");
         STATE_MAP.put("Delaware", "DE");
-        STATE_MAP.put("NewYork", "NY");
-        STATE_MAP.put("NorthCarolina", "NC");
+        STATE_MAP.put("New York", "NY");
+        STATE_MAP.put("North Carolina", "NC");
         STATE_MAP.put("Florida", "FL");
-        STATE_MAP.put("NorthDakota", "ND");
+        STATE_MAP.put("North Dakota", "ND");
         STATE_MAP.put("Georgia", "GA");
         STATE_MAP.put("Ohio", "OH");
         STATE_MAP.put("Hawaii", "HI");
@@ -65,11 +65,11 @@ public class Person {
         STATE_MAP.put("Illinois", "IL");
         STATE_MAP.put("Pennsylvania", "PA");
         STATE_MAP.put("Indiana", "IN");
-        STATE_MAP.put("RhodeIsland", "RI");
+        STATE_MAP.put("Rhode Island", "RI");
         STATE_MAP.put("Iowa", "IA");
-        STATE_MAP.put("SouthCarolina", "SC");
+        STATE_MAP.put("South Carolina", "SC");
         STATE_MAP.put("Kansas", "KS");
-        STATE_MAP.put("SouthDakota", "SD");
+        STATE_MAP.put("South Dakota", "SD");
         STATE_MAP.put("Kentucky", "KY");
         STATE_MAP.put("Tennessee", "TN");
         STATE_MAP.put("Louisiana", "LA");
@@ -83,7 +83,7 @@ public class Person {
         STATE_MAP.put("Michigan", "MI");
         STATE_MAP.put("Washington", "WA");
         STATE_MAP.put("Minnesota", "MN");
-        STATE_MAP.put("WestVirginia", "WV");
+        STATE_MAP.put("West Virginia", "WV");
         STATE_MAP.put("Mississippi", "MS");
         STATE_MAP.put("Wisconsin", "WI");
     }
